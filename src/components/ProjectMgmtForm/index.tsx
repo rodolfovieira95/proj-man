667c4de586560679375eba82156bcef72448b8fc
Rodolfo Vieira <rodolfovieira95@outlook.com>
2025-01-12 14:34:16 -0300

    feat: implements ProjectMgmtForm  component

---
+++ src/components/ProjectMgmtForm/index.tsx
@@ -0,0 +1,59 @@
+"use client";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { Textarea } from "../ui/textarea";
+
+const ProjectMgmtForm = () => {
+  const form = useForm();
+
+  return (
+    <Form {...form}>
+      <FormField
+        control={form.control}
+        name="title"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Título do Projeto</FormLabel>
+            <FormControl>
+              <Input placeholder="Card Title" {...field} />
+            </FormControl>
+            <FormDescription />
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="description"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Descrição do Projeto</FormLabel>
+            <FormControl>
+              <Textarea
+                placeholder="Tell us a little bit about this card"
+                className="resize-none"
+                {...field}
+              />
+            </FormControl>
+            <FormDescription />
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+
+      <Button variant="outline">Cancelar</Button>
+      <Button type="submit">Enviar</Button>
+    </Form>
+  );
+};
+export default ProjectMgmtForm;
