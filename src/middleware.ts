cdfb39abe0844490ef6037ea28982fddb47b998c
Rodolfo Vieira <rodolfovieira95@outlook.com>
2025-01-12 00:55:02 -0300

    feat: implements middleware to control routes

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+import { auth } from "./auth";
+
+export async function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+  const session = await auth();
+
+  if (pathname.startsWith("/dashboard")) {
+    if (!session?.user) {
+      const loginUrl = new URL("/login", req.url);
+      loginUrl.searchParams.set("callbackUrl", req.url);
+      return NextResponse.redirect(loginUrl);
+    }
+  }
+  if (pathname === "/" || (pathname === "/login" && session?.user)) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/dashboard", "/project/:path*", "/"],
+};
