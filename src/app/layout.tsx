f297ba1e7767484c6c861e59102b29985996b5df
Rodolfo Vieira <rodolfovieira95@outlook.com>
2025-01-16 02:42:24 -0300

    feat: installs toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from "@/components/Providers/ThemeProvider";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { SessionProvider } from "next-auth/react";
+import { Toaster } from "@/components/ui/toaster";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -41,6 +42,7 @@ export default function RootLayout({
           >
             <Header />
             {children}
+            <Toaster />
             <Footer />
           </ThemeProvider>
         </SessionProvider>
