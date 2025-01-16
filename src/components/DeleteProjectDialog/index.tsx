ce352ae9bd32c38913b901cf629bd1888a20806c
Rodolfo Vieira <rodolfovieira95@outlook.com>
2025-01-16 01:06:44 -0300

    feat: creates DeleteProjectDialog component

---
+++ src/components/DeleteProjectDialog/index.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { redirect } from "next/navigation";
+import { deleteProject } from "@/actions/projects";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+const DeleteProjectDialog = ({ projectId }: { projectId: string }) => {
+  const handleDeleteButton = async () => {
+    try {
+      await deleteProject(projectId);
+      redirect("/dashboard");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="destructive">Delete Project</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Tem certeza que deseja deletar o projeto?</DialogTitle>
+          <DialogClose />
+        </DialogHeader>
+        <DialogClose asChild>
+          <Button>Cancelar</Button>
+        </DialogClose>
+        <Button variant="destructive" onClick={handleDeleteButton}>
+          Deletar
+        </Button>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default DeleteProjectDialog;
