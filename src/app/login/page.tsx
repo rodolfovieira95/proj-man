9164394d947f72c0c988f7a25a3c5d3bdbb364ad
Rodolfo Vieira <rodolfovieira95@outlook.com>
2025-01-26 02:50:01 -0300

    feat: updates login page layout

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,6 @@
 import { signIn } from "@/auth";
 import PasskeyLogin from "@/components/PasskeyLogin";
+import { Button } from "@/components/ui/button";
 
 const SigninPage = () => {
   const handleSignIn = async (provider: string, formData?: FormData) => {
@@ -14,30 +15,35 @@ const SigninPage = () => {
   };
 
   return (
-    <main className="px-8">
-      <h1 className="font-bold text-xl">Login</h1>
-      <form
-        action={async (formData) => {
-          "use server";
-          await handleSignIn("resend", formData);
-        }}
-        className="flex gap-4 mt-6"
-      >
-        <input type="text" name="email" placeholder="Email" />
-        <button type="submit">Sign in with Resend</button>
-      </form>
+    <main className="flex min-h-dvh">
+      <div className="px-8 w-[40%] mt-8 flex flex-col justify-center items-center">
+        <h1 className="text-5xl font-bold mb-8">ProjMan</h1>
+        <p className="mb-8">Login using social media to get quick access</p>
+        <form
+          action={async () => {
+            "use server";
+            await handleSignIn("github");
+          }}
+          className="flex gap-4 mt-4"
+        >
+          <Button type="submit">Signin with GitHub</Button>
+        </form>
+        <PasskeyLogin />
+      </div>
 
-      <form
-        action={async () => {
-          "use server";
-          await handleSignIn("github");
-        }}
-        className="flex gap-4 mt-4"
-      >
-        <button type="submit">Signin with GitHub</button>
-      </form>
-
-      <PasskeyLogin />
+      <div className="flex flex-col justify-center items-center w-[60%] bg-accent px-8">
+        <h1 className="text-center font-bold text-xl mt-8">Login com Email</h1>
+        <form
+          action={async (formData) => {
+            "use server";
+            await handleSignIn("resend", formData);
+          }}
+          className="flex flex-col gap-4 mt-6"
+        >
+          <input type="text" name="email" placeholder="Email" />
+          <Button type="submit">Sign in with Resend</Button>
+        </form>
+      </div>
     </main>
   );
 };
