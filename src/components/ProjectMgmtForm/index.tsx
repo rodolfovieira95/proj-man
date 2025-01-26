599deeb4116399dbbaee12b13c07846bd7b75de6
Rodolfo Vieira <rodolfovieira95@outlook.com>
2025-01-26 02:52:53 -0300

    feat: implements project info form

--- src/components/ProjectMgmtForm/index.tsx
+++ src/components/ProjectMgmtForm/index.tsx
@@ -13,54 +13,79 @@ import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import { Textarea } from "../ui/textarea";
 import DeleteProjectDialog from "../DeleteProjectDialog";
+import { updateProjectInfo } from "@/actions/projects";
+import { useToast } from "@/hooks/use-toast";
 
 const ProjectMgmtForm = ({ projectId }: { projectId: string }) => {
+  const { toast } = useToast();
   const form = useForm({
     defaultValues: {
-      title: "",
+      name: "",
       description: "",
     },
   });
 
+  const onSubmit = async (data: { name: string; description: string }) => {
+    try {
+      await updateProjectInfo({
+        id: projectId,
+        title: data.name,
+        description: data.description,
+      });
+      toast({
+        title: "Sucesso!",
+        description: "Projeto alterado com sucesso!",
+      });
+    } catch (error) {
+      console.error("Erro ao criar projeto:", error);
+      toast({
+        title: "Erro!",
+        description: "Erro ao alterar dados do projeto!",
+      });
+    }
+  };
+
   return (
     <Form {...form}>
-      <FormField
-        control={form.control}
-        name="title"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>Título do Projeto</FormLabel>
-            <FormControl>
-              <Input placeholder="Card Title" {...field} />
-            </FormControl>
-            <FormDescription />
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-      <FormField
-        control={form.control}
-        name="description"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>Descrição do Projeto</FormLabel>
-            <FormControl>
-              <Textarea
-                placeholder="Tell us a little bit about this card"
-                className="resize-none"
-                {...field}
-              />
-            </FormControl>
-            <FormDescription />
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-      <div className="flex gap-4">
-        <Button variant="outline">Cancelar</Button>
-        <Button type="submit">Enviar</Button>
-      </div>
-      <DeleteProjectDialog projectId={projectId} />
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Nome do Projeto</FormLabel>
+              <FormControl>
+                <Input placeholder="Titulo do projeto" {...field} />
+              </FormControl>
+              <FormDescription />
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Descrição do Projeto</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Tell us a little bit about this card"
+                  className="resize-none"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription />
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="flex gap-4">
+          <Button variant="outline">Cancelar</Button>
+          <Button type="submit">Enviar</Button>
+        </div>
+        <DeleteProjectDialog projectId={projectId} />
+      </form>
     </Form>
   );
 };
